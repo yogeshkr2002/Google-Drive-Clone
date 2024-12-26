455649572b89f4b370c411d94251a3607796b1c0
yogeshkr2002 <yogeshkumarsharma689@gmail.com>
2024-12-26 14:58:31 +0530

    Share,delete,rename and all the rest functionality is updated

--- components/Card.tsx
+++ components/Card.tsx
@@ -18,9 +18,7 @@ const Card = ({ file }: { file: Models.Document }) => {
         />
 
         <div className="flex flex-col items-end justify-between">
-          <ActionDropdown
-          //   file={file}
-          />
+          <ActionDropdown file={file} />
           <p className="body-1">{convertFileSize(file.size)}</p>
         </div>
       </div>
